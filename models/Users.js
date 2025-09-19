d998751aa8f7b7c4819f6bcf00327ff39c766690
amir-saeedi <saeediamir1381@gmail.com>
2025-09-19 18:03:00 +0330

    fix build

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    _id: { type: string, require: true },
-    name: { type: string, require: true },
-    email: { type: string, require: true, unique: true },
-    imageUrl: { type: string, require: true },
+    _id: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    imageUrl: { type: String, require: true },
     cartItems: { type: Object, default: {} },
 }, { minimize: false })
 
