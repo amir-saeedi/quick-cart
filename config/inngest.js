d69bc32ab0e72d2d8eff75957b7492d7b1dd8adc
amir-saeedi <saeediamir1381@gmail.com>
2025-09-21 16:48:25 +0330

    fix order api

--- config/inngest.js
+++ config/inngest.js
@@ -53,7 +53,7 @@ export const syncUserDeletion = inngest.createFunction(
 
 // Function for user orders
 export const createUserOrder = inngest.createFunction(
-    { id: "create-user-order", batchEvents: { maxSize: 25, timeout: '5s' } },
+    { id: "create-user-order", batchEvents: { maxSize: 5, timeout: '5s' } },
     { event: "order/created" },
     async ({ events }) => {
         const orders = events.map((event) => {
